b64dd9cc0a276d8175d46a8fd29040d2ac1a9f1c
PhuocDuy <nguyenphuocduy2908@gmail.com>
2023-06-19 00:20:49 +0700

    addFoodPopup1

--- src/screens/menu.js
+++ src/screens/menu.js
@@ -1,14 +1,64 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { ScrollView } from "react-native";
 const Menu = () => {
   return (
-    <View>
-      <Text>Menu</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.box}>
+        <Text style={styles.text1}>Bia</Text>
+        <Text style={styles.text2}>20000/chai</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text1}>Bia</Text>
+        <Text style={styles.text2}>20000/chai</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text1}>Bia</Text>
+        <Text style={styles.text2}>20000/chai</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text1}>Bia</Text>
+        <Text style={styles.text2}>20000/chai</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text1}>Bia</Text>
+        <Text style={styles.text2}>20000/chai</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text1}>Bia</Text>
+        <Text style={styles.text2}>20000/chai</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text1}>Bia</Text>
+        <Text style={styles.text2}>20000/chai</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text1}>Bia</Text>
+        <Text style={styles.text2}>20000/chai</Text>
+      </View>
+    </ScrollView>
   );
 };
 
 export default Menu;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  box: {
+    marginTop: 20,
+    marginHorizontal: 20,
+    backgroundColor: "#c3c3c3",
+    height: 80,
+    display: "flex",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  text1: {
+    color: "#644AB5",
+    fontSize: 25,
+    fontWeight: 700,
+  },
+  text2: {
+    color: "#644AB5",
+    fontSize: 20,
+  },
+});
--- src/screens/table.js
+++ src/screens/table.js
@@ -1,14 +1,71 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { ScrollView } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 const Tables = () => {
   return (
-    <View>
-      <Text>Tables</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 1</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 2</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 3</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 4</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 5</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 6</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 7</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 8</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 9</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Table 10</Text>
+        <Entypo name="dots-three-horizontal" size={24} color="#644AB5" />
+      </View>
+    </ScrollView>
   );
 };
 
 export default Tables;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  box: {
+    marginTop: 20,
+    marginHorizontal: 20,
+    backgroundColor: "#c3c3c3",
+    height: 80,
+    display: "flex",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text: {
+    color: "#644AB5",
+    fontSize: 25,
+    fontWeight: 700,
+  },
+});
