bc94a59c5807101e9218d8a34dd682b4b17b1c12
ncsfanbros2001 <ncssuperfan@gmail.com>
2023-06-20 00:39:38 +0700

    Orders and Revenue

---
+++ src/screens/orderDetails.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ordersDetails = () => {
+    return (
+        <View>
+            <Text>Revenue</Text>
+        </View>
+    );
+};
+
+export default ordersDetails;
+
+const styles = StyleSheet.create({});
